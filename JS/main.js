370262a29a627cb386eaab7b62c790c55d9abef9
Saadakhtaraptech <Saadakhtaraptech@gmail.com>
2025-08-06 23:00:33 -0700

    work

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+//arithmatic ooprator//
+
+let num1 =prompt("Enter Your First Value")
+let num2 =prompt("Enter Your Second Value")
+let result = num1 + num2
+// let result = num1 - num2
+// let result = num1 * num2
+document.getElementById("Para").innerHTML=result
+
+
+
+//   let a = 8
+//         let b = 5
+
+//         let result = a++ + b + ++ a + a + --b + a++
+//         document.write(result)
