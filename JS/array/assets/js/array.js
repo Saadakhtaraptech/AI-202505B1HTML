0883cddff5a6c37a9e51228e8a214305ee3b4353
Saadakhtaraptech <Saadakhtaraptech@gmail.com>
2025-08-27 23:01:25 -0700

    28/aug

--- JS/array/assets/js/array.js
+++ JS/array/assets/js/array.js
@@ -1,12 +1,49 @@
 // Array
 
-let color = ["red", "green", "blue", "yellow", "black", "white"];
+// let color = ["red", "green", "blue", "yellow", "black", "white"];
 
-document.write(color[2],"<br>",color[4],"<br>",color[3]<br>,"&nbsp;""&nbsp",color[5],"<br>")
-document.write(color.length)
+// document.write(color[2],"<br>",color[4],"<br>",color[3],"<br>",color[5],"<br>")
+// document.write(color.length)
 // document.write(color[0], "<br>")
 // document.write(color[1], "<br>")
 // document.write(color[2], "<br>")
 // document.write(color[3], "<br>")
 // document.write(color[4], "<br>")
 // document.write(color[5], "<br>")
+
+
+
+// multi dimensional array
+
+// let courses = [
+//     ["html", "css", "js", "bootstrap", "jquery"] ,
+//     ["xml", "json", "adv.js", "mysql", "php"],
+//     ["sql server", "c#", "net", "angular"],
+// ]
+// document.write(courses[0][3], "<br>", courses[1][1], "<br>")
+// console.log(courses)
+
+
+// document.write(courses)
+// for(let value of courses){
+//     document.write(value[2], "<br>" [0], "<br>")
+// }
+
+// for(value of courses){
+//     for (data of value){
+//         document.write(data, "<br>")
+//     }
+// }
+
+
+
+// method array
+
+let color = ["red", "green", "blue", "yellow", "black", "white"];
+
+color.push("purple")  // add last
+color.unshift("pink") // add first
+color.pop()          // remove last
+color.shift()       // remove first
+color.splice(2, 3) // remove specific index
+document.write(color, "<br>")
