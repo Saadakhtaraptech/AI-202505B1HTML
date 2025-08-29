7f7ff36c6110757d778490535b1dc2d9a7b071c8
saadakhtaraptech <saadakhtaraptech@gmail.com>
2025-08-29 22:49:13 +0500

    as

--- JS/array/assets/js/array.js
+++ JS/array/assets/js/array.js
@@ -2,7 +2,7 @@
 
 // let color = ["red", "green", "blue", "yellow", "black", "white"];
 
-// document.write(color[2],"<br>",color[4],"<br>",color[3],"<br>",color[5],"<br>")
+// document.write(color[2],"<br>",color[4],"<br>",color[3],"<br>",color[5],"<br>",color[0],"<br>",color[1],"<br>")
 // document.write(color.length)
 // document.write(color[0], "<br>")
 // document.write(color[1], "<br>")
@@ -20,8 +20,8 @@
 //     ["xml", "json", "adv.js", "mysql", "php"],
 //     ["sql server", "c#", "net", "angular"],
 // ]
-// document.write(courses[0][3], "<br>", courses[1][1], "<br>")
-// console.log(courses)
+// // document.write(courses[0][3], "<br>", courses[1][1], "<br>")
+// // console.log(courses)
 
 
 // document.write(courses)
@@ -41,9 +41,74 @@
 
 let color = ["red", "green", "blue", "yellow", "black", "white"];
 
-color.push("purple")  // add last
-color.unshift("pink") // add first
-color.pop()          // remove last
-color.shift()       // remove first
-color.splice(2, 3) // remove specific index
+// color.push("purple")  // add last
+// color.unshift("pink") // add first
+// color.pop()          // remove last
+// color.shift()       // remove first
+// color.splice(2, 3) // remove specific index
+// color.splice(2, 0, "orange", "brown") // add specific index
+// color.sort()       // sort array
+// color.reverse()    // reverse array
+// color.indexOf("green") // find index of element
+// color.includes("blue") // check if element exists
 document.write(color, "<br>")
+
+
+
+
+// // assignment
+
+
+//  // 2D array (array ke andar array)
+// //   let colors = [
+// //     ["red", "green", "blue"],
+// //     ["yellow", "black", "white"]
+// //   ];
+
+// //   document.write("<table border='1' cellspacing='0' cellpadding='5'>");
+
+// //   // Outer loop (rows)
+// //   for (let row of colors) {
+// //     // document.write("<tr>");
+    
+// //     // Inner loop (columns)
+// //     for (let col of row) {
+// //       document.write("<td>" + col + "</td>");
+// //     }
+
+// //     document.write("</tr>");
+// //   }
+
+
+
+// // 2222222
+
+
+
+// let players = [
+//     { name: "HTML", Duration: "Three Months" },
+//     { name: "JAVA SCRIPT", Duration: "Four Months" },
+//     { name: "BOOTSTRAP", Duration: "Three Months" },
+//     { name: "JQUERY", Duration: "Four Months" },
+//     { name: "ANGULAR", Duration: "Four Months" }
+//   ];
+
+//   // Start table
+//   document.write("<table border='1' cellspacing='0' cellpadding='5'>");
+
+//   // Table header
+//   document.write("<tr><th>S.NO</th><th>Course Name</th><th>Role</th></tr>");
+
+//   // Loop through players
+//   let sno = 1;
+//   for (let p of players) {
+//     // document.write("<tr>");
+//     document.write("<td>" + sno + "</td>");
+//     document.write("<td>" + p.name + "</td>");
+//     document.write("<td>" + p.Duration + "</td>");
+//     document.write("</tr>");
+//     sno++;
+  // }
+
+  // End table
+//   document.write("</table>");
