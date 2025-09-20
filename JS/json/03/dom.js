25913c5bcd2737671c82bb6e108cf469d68db886
Saadakhtaraptech <Saadakhtaraptech@gmail.com>
2025-09-19 23:01:19 -0700

    20

--- JS/json/03/dom.js
+++ JS/json/03/dom.js
@@ -1,1 +1,12 @@
-let para = document.getElementById("para")
+// let para = document.getElementsByClassName("para")
+// para[0].innerHTML = "HELLO SAAD"
+// para[2].style.color = "red"
+
+
+// let para = document.getElementsByTagName("p")
+// para[0].innerHTML = "hello muzammil"
+// para[2].style.color = "green"
+
+// let para = document.querySelector("*")
+// para.style.color = "GREEN"
+
