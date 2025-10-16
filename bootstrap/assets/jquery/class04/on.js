26dbe4c0e86b0d6bd0437add6984d34a9cd5fda6
SFC EXAM <sfcexam@LABSFC.COM>
2025-10-15 22:57:49 -0700

    16

---
+++ bootstrap/assets/jquery/class04/on.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("p").on({
+        mouseenter : function(){
+            $(this).css({
+                "background color" : "black",
+                "color" : "white"
+            })
+        },
+
+          mouse : function(){
+            $(this).css({
+                "background color" : "black",
+                "color" : "white"
+            })
+        }
+    })
+})
